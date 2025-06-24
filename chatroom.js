032e28b721fc117e7830347adf206dc7576c26e9
Anurag <anurag789p@gmail.com>
2025-06-24 12:29:55 +0530

    Refactor ChatRoom methods for improved logging and add message broadcasting on client join/leave events; update index.html for enhanced terminal connection handling and user feedback.

--- chatroom.js
+++ chatroom.js
@@ -2,19 +2,20 @@ import { Room } from "colyseus";
 
 export default class ChatRoom extends Room {
     onCreate(options) {
-        console.log("ChatRoom created!!!!",options);
+        console.log("ChatRoom created!!!!", options);
     }
 
     onDispose(options) {
-        console.log("ChatRoom destroyed!!!!",options);
+        console.log("ChatRoom destroyed!!!!", options);
     }
 
     onJoin(client) {
         console.log(`client ${client.sessionId} joined the room`);
+        this.broadcast("message", `client ${client.sessionId} joined the room`);
     }
     
     onLeave(client) {
         console.log(`client ${client.sessionId} left the room`);
+        this.broadcast("message", `client ${client.sessionId} left the room`);
     }
-
 }
